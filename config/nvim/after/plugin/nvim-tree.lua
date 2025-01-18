15e428542222fcb7c94bfac37785d856c2200f6f
junsee1412 <dat10319@gmail.com>
2025-01-18 21:38:57 +0700

    remove old config

--- config/nvim/after/plugin/nvim-tree.lua
+++
@@ -1,8 +0,0 @@
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-
-require("nvim-tree").setup({
-    auto_reload_on_write = true
-})
-vim.keymap.set('n', '<c-b>', ':NvimTreeFindFileToggle<CR>')
-vim.keymap.set('n', '<c-b-f>', ':NvimTreeFocus<CR>')
--- config/nvim/lua/core/mappings.lua
+++
@@ -1,2 +0,0 @@
-vim.g.mapleader = " " -- easy to reach leader key
-vim.keymap.set("n", "-", vim.cmd.Ex) -- need nvim 0.8+
